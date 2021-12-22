5240fdb0c15708ec6368e274e1c051f8cf00b4dc
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2021-12-22 22:05:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (9.93%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool matching(int a,int b){
+        return (a=='(' && b==')' || a=='{' && b=='}' || a=='[' && b==']' );
+    }
+    bool isValid(string s) {
+        stack<int>st;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(' or s[i]=='{' or s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty())
+                    return false;
+                else if(matching(st.top(),s[i])==false){
+                    return false;
+                }
+                else
+                    st.pop();
+            }
+        }
+        return st.empty()==true;
+    }
+};
