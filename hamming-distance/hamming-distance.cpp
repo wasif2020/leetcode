6ec076ebce5acb3ce29430ad9bd522d6c06b45ba
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2021-12-24 03:26:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (27.75%) - LeetHub

---
+++ hamming-distance/hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count=0;
+        for(int i=0; i<32; i++){
+            if(x%2!=y%2)
+                count++;
+            x=x>>1;
+            y=y>>1;
+        }
+        return count;
+    }
+};
