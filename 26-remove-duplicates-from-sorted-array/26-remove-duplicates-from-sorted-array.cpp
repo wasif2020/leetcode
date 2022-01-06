8ea1c4ea84312e87568f915e026d7c399b424fc2
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-01-06 20:03:37 +0530

    Time: 10 ms (68.71%), Space: 18.3 MB (73.54%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+     int res=1;
+        if(nums.size()<1){
+            return 0;
+        }
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]!=nums[res-1]){
+                nums[res]=nums[i];
+                res++;
+            }
+        }
+        return res;
+    }
+};
