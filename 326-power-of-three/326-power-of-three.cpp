7f10b93b5aba61f68d212bb668cf14195c427673
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-01-13 01:37:03 +0530

    Time: 79 ms (5.01%), Space: 6.2 MB (7.70%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        for(int i=0; i<n; i++){
+            if(pow(3,i)>n)
+                return false;
+            if(pow(3,i)==n)
+                return true;
+        }
+        return false;
+    }
+};
