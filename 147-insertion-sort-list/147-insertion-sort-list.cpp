e7fb1a1feafefcf56d8a9f5ec53f3189f6b9adef
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-01-23 02:46:16 +0530

    Time: 16 ms (90.61%), Space: 10.7 MB (6.40%) - LeetHub

---
+++ 147-insertion-sort-list/147-insertion-sort-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        multiset<int>s;
+        ListNode*curr=head;
+        if(head==NULL)
+            return NULL;
+        while(curr!=NULL){
+            s.insert(curr->val);
+            curr=curr->next;
+        }
+        curr=head;
+        for(auto x:s){
+            curr->val=x;
+            curr=curr->next;
+        }
+        return head;
+    }
+};
