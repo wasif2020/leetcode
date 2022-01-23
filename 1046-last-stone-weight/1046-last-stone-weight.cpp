0ce81464b7a0733103c118adf5e547292386a42f
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-01-23 22:09:06 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (97.60%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq(stones.begin(),stones.end());
+        while(pq.size()>=2){
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(x==y){
+                if(pq.empty())
+                    pq.push(0);
+            }
+            if(x<y){
+                pq.push(y-x);
+            }
+        }
+        return pq.top();
+    }
+};
