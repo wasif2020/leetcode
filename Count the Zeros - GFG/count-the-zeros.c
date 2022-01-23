55bd20b00367282bd86f34d036b43ead9eeab3ed
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-01-24 01:44:26 +0530

    Added solution - LeetHub

---
+++ Count the Zeros - GFG/count-the-zeros.c
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int countZeroes(int arr[], int n) {
+    int count=0;
+    for(int i=n-1; i>=0; i--){
+        if(arr[i]!=0){
+            return count;
+        }
+        count++;
+    }
+    return count;
+}
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);;
+        }
+        
+        int ans = countZeroes(arr, n);
+        printf("%d\n", ans);
+    }
+    return 0;
+}
+  // } Driver Code Ends
