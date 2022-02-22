89148cde03ca759eb6423dceff5203e49619fbbb
MD WASIF <59055767+wasif2020@users.noreply.github.com>
2022-02-22 11:26:03 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.67%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string col) {
+        int s=col.length();
+        int ans=0,c=0;
+        for(int i=s-1;i>=0;i--){
+            ans+=pow(26,c++)*(col[i]-64);
+        }
+        return ans;
+    }
+};
