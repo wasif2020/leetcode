94dd75a09e0b4c4fa576001d9fa4fd55eaab1687
WASIF <59055767+wasif2020@users.noreply.github.com>
2022-06-12 12:41:59 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (59.88%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int it=haystack.find(needle);
+        if(it!=string::npos)
+            return it; 
+        
+        return -1;
+    }
+};
