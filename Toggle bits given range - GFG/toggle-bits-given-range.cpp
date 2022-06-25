129c5d0f80fbc37adc00d2ab339f80539e26e738
WASIF <59055767+wasif2020@users.noreply.github.com>
2022-06-26 02:14:41 +0530

    Added solution - LeetHub

---
+++ Toggle bits given range - GFG/toggle-bits-given-range.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    int toggleBits(int N , int L , int R) {
+        if(L==0 and R==0){
+            return N;
+        }
+        while(L<=R){
+            N=N^(1<<(L-1));
+            L++;
+        }
+        return N;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,L,R;
+        
+        cin>>N>>L>>R;
+
+        Solution ob;
+        cout << ob.toggleBits(N,L,R) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
