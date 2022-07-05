171e4d100535767fb33bca61cee8ec615c91316a
WASIF <59055767+wasif2020@users.noreply.github.com>
2022-07-05 17:13:42 +0530

    Time: 4 ms (22.52%), Space: 6.2 MB (30.85%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
-    int memo[46]={0};
+    int solve(int n, vector<int>&dp){
+        if(n<=2)
+            return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
     int climbStairs(int n) {
-     // USING TABULATION METHOD
-         if(memo[n]==0){
-             int res;
-             if(n<=2)
-                 res=n;
-             else 
-                 res=climbStairs(n-1)+climbStairs(n-2);
-             memo[n]=res;
-         }
-        return memo[n];
+       //recursive solution----
+        if(n<=2)
+            return n;
+        vector<int>dp(n,-1);
+        return solve(n-1,dp)+solve(n-2,dp);
     }
 };
