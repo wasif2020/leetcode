daef1cca3ef33b89193fff6197894b25d79adb5d
WASIF <59055767+wasif2020@users.noreply.github.com>
2022-10-20 00:06:36 +0530

    Time: 39 ms (50.57%), Space: 19.6 MB (60.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
+    int majority(vector<int>&nums){
+        int n=nums.size();
+        int count=1,res=0;
+        for(int i=1; i<n; i++){
+            if(nums[res]==nums[i])
+                count++;
+            else
+                count--;
+            if(count==0){
+                res=i;
+                count=1;
+            }
+        }
+        return nums[res];
+    }
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int>m;
+     /*
+     solution 1
+     unordered_map<int,int>m;
             int n=nums.size();
             for(auto x:nums)
                 m[x]++;
@@ -9,7 +26,19 @@ public:
             if(x.second>n/2)
                 return x.first;
         
-        return -1;
+        return -1;*/
         
+        //better solution | without using extra space;
+        //step 1 find which is majority element;
+        //step 2 verify if found element is majority or not;
+        int maj=majority(nums);
+        int count=0;
+        int n=nums.size();
+        for(auto x:nums)
+            if(x==maj)
+                count++;
+        if(count>n/2)
+            return maj;
+        return -1;
     }
 };
