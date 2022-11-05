0b73b3e5dabf11be18d8f0ca9742b3eb336e2e92
WASIF <59055767+wasif2020@users.noreply.github.com>
2022-11-05 22:17:11 +0530

    Time: 10 ms (23.73%), Space: 8.1 MB (15.08%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -2,29 +2,26 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         unordered_map<int,int>m;
-        unordered_set<int>s;
-        for(auto x:arr2)
-            s.insert(x);
-        for(auto x:arr1)
-            m[x]++;
+        unordered_set<int>s(arr2.begin(),arr2.end());
         vector<int>v;
-        for(auto x: arr2)
-        {
-            auto it=m.find(x);
-            if(it!=m.end()){
-                for(int i=0; i<it->second; i++)
-                    v.push_back(x);
-            }
-        }
-        vector<int>vi;
+        vector<int>v1;
         for(auto x:arr1){
             if(s.find(x)==s.end()){
-                    vi.push_back(x);
+                v1.push_back(x);
+                continue;
+            }
+            m[x]++;
+        }
+        for(auto x:arr2){
+            auto it=m.find(x);
+            for(int i=0; i<it->second; i++){
+                v.push_back(x);
             }
         }
-        sort(vi.begin(),vi.end());
-        for(auto x:vi)
+        sort(v1.begin(),v1.end());
+        for(auto x:v1){
             v.push_back(x);
+        }
         return v;
     }
 };
