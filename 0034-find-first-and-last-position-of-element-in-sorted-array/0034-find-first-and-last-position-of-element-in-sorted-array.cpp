4820cbdc702708f026325712707ae613545d5406
WASIF <59055767+wasif2020@users.noreply.github.com>
2024-02-24 21:07:06 +0530

    Time: 7 ms (53.45%), Space: 16.1 MB (7.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-     vector<int>v;
-        int str=-1;
-        int end=-1;
+        vector<int>res(2);
+        res[0]=-1;
+        res[1]=-1;
         int n=nums.size();
-        if(n==0){
-            v.push_back(-1);
-            v.push_back(-1);
-            return v;
-        }
-        int s=0;
-        int e=n-1;
-        while(s<=e){
-            int m=(s+e)/2;
-            if(nums[m]==target){
-                str=m;
-                end=m;
+        if(n==0) return res;
+        int mid,left,right;
+        int start=0,end=n-1;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(nums[mid]==target) {
+                left=mid;
                 break;
             }
-            else if(nums[m]<target){
-                s=m+1;
-            }
-            else{
-                e=m-1;
-            }
+            else if(nums[mid]>target) end=mid-1;
+            else start=mid+1;
         }
-        int idx=str;
-        for(int i=str-1; i>=0; i--){
-            if(nums[i]==target){
-                str--;
-            }
-            else{
-                break;
-            }
-        }
-        for(int i=end+1; i<n; i++){
-            if(nums[i]==target){
-                end++;
-            }
-            else{
-                break;
-            }
-        }
-        v.push_back(str);
-        v.push_back(end);
-        return v;
+        if(start>end) return res;
+        right=left;
+        while(left>=0 and nums[left]==target) res[0]=left--;
+        while(right<n and nums[right]==target) res[1]=right++;
+        
+        return res;
     }
 };
