ee9d29752d3d3ab84be3ea29f4892f7be29e5222
WASIF <59055767+wasif2020@users.noreply.github.com>
2024-02-26 16:54:52 +0530

    Time: 20 ms (48.06%), Space: 34 MB (39.60%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+         int n=nums.size();
+        if(n==1) return 0;
+        int lsum=0, rsum=0;
+        for(int i=0;i<n; i++)
+            rsum+=nums[i];
+        
+        for(int i=0;i<n; i++){
+            rsum=rsum-nums[i];
+            if(lsum==rsum) return i;
+            lsum=lsum+nums[i];
+        }
+        
+        return -1;
+    }
+};
