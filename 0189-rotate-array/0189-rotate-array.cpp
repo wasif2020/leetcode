36601d7c100d3212c260f860840bdaf87f83a31a
Wasif <59055767+wasif2020@users.noreply.github.com>
2024-08-18 00:10:59 +0530

    Time: 20 ms (61.31%), Space: 27.4 MB (72.25%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    void reverse(vector<int>&nums,int low, int high){
-        while(low<high){
-            swap(nums[low++],nums[high--]);
+    void reverse(vector<int>&nums,int s,int e){
+        while(s<e){
+            swap(nums[s++],nums[e--]);
         }
     }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        if(k==0) return;
-        reverse(nums,0,n-k-1);
         reverse(nums,n-k,n-1);
+        reverse(nums,0,n-k-1);
         reverse(nums,0,n-1);
-        
     }
 };
