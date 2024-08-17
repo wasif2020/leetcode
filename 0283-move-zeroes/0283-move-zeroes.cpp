e896e1043287e42b7263d7c67399c41ad490250e
Wasif <59055767+wasif2020@users.noreply.github.com>
2024-08-17 23:42:04 +0530

    Time: 9 ms (94.03%), Space: 21.9 MB (21.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size(),count=0;        
-        for(int i=0; i<n; i++)
-            if(nums[i]!=0) swap(nums[i],nums[count++]);
+        int count=0;
+        int n=nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                int temp=nums[count];
+                nums[count++]=nums[i];
+                nums[i]=temp;
+            }
+        }
+        
     }
 };
